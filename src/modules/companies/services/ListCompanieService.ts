8f5ccec9b0d4979e51a7bc2a13ad6e5d265c0a25
Vilhenna Jr <vilhennajroficial@gmail.com>
2023-01-12 19:56:21 -0400

    paginate list companie

--- src/modules/companies/services/ListCompanieService.ts
+++ src/modules/companies/services/ListCompanieService.ts
@@ -2,13 +2,24 @@ import { getCustomRepository } from 'typeorm';
 import Companie from '../typeorm/entities/Companie';
 import CompanieRepository from '../typeorm/repositories/CompaniesRepository';
 
+interface IPaginateCompanie {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  prev_page: number | null;
+  next_page: number | null;
+  data: Companie[];
+}
+
 class ListCompanieService {
-  public async execute(): Promise<Companie[]> {
+  public async execute(): Promise<IPaginateCompanie> {
     const companiesRepository = getCustomRepository(CompanieRepository);
 
-    const companies = companiesRepository.find();
+    const companies = await companiesRepository.createQueryBuilder().paginate();
 
-    return companies;
+    return companies as IPaginateCompanie;
   }
 }
 
